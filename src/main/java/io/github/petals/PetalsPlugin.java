2cc7e7fe62030a9ff564574090a3bd82e276bc2c
Joseph <joseph300905@gmail.com>
2022-08-09 11:47:28 +0200

    Initial commit

---
+++ src/main/java/io/github/petals/PetalsPlugin.java
@@ -0,0 +1,11 @@
+package io.github.petals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PetalsPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        this.getLogger().info("Hello, World!");
+    }
+}
+
