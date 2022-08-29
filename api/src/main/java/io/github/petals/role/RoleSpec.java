434f88957ef1deabb36284419bd2da21dcf677b6
Joseph <joseph300905@gmail.com>
2022-08-29 18:31:08 +0200

    Experimental awesome breaking API changes

---
+++ api/src/main/java/io/github/petals/role/RoleSpec.java
@@ -0,0 +1,14 @@
+package io.github.petals.role;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface RoleSpec {
+    public String name();
+    public String description();
+}
+
--- src/main/java/io/github/petals/structures/PetalsPlayer.java
+++ src/main/java/io/github/petals/structures/PetalsPlayer.java
@@ -1,6 +1,8 @@
 package io.github.petals.structures;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Proxy;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
@@ -11,6 +13,8 @@ import org.bukkit.entity.Player;
 import io.github.petals.Game;
 import io.github.petals.Metadata;
 import io.github.petals.role.Role;
+import io.github.petals.role.RoleMeta;
+import io.github.petals.role.RoleSpec;
 import redis.clients.jedis.JedisPooled;
 
 public class PetalsPlayer<T extends Role> implements Game.Player<T> {
@@ -57,11 +61,65 @@ public class PetalsPlayer<T extends Role> implements Game.Player<T> {
         String roleClass = pooled.hget(uniqueId, "role");
         if (roleClass != null) {
             try {
-                Class<?> role = Class.forName(pooled.hget(uniqueId, "role"));
+                final Class<?> role = Class.forName(pooled.hget(uniqueId, "role"));
                 if (!Role.class.isAssignableFrom(role)) throw new ClassCastException(String.format("%s does not implement Role", role.getName()));
+                if (!role.isInterface()) throw new ClassCastException(String.format("%s is not an interface", role.getName()));
 
-                this.role = (T) role.getConstructor(Game.Player.class).newInstance(this);
-            } catch (ClassNotFoundException | ClassCastException | InvocationTargetException | IllegalAccessException | InstantiationException | NoSuchMethodException e) {
+                final RoleSpec spec = role.getDeclaredAnnotation(RoleSpec.class);
+                if (spec == null) throw new ClassCastException(String.format("%s is not annotated with io.github.petals.role.RoleSpec", role.getName()));
+
+                T instance = (T) Proxy.newProxyInstance(
+                    role.getClassLoader(),
+                    new Class[] { role },
+                    (proxy, method, args) -> {
+                        switch (method.getName()) {
+                            case "player":
+                                return this;
+                            case "name":
+                                return spec.name();
+                            case "description":
+                                return spec.description();
+                        }
+
+                        final RoleMeta meta = method.getDeclaredAnnotation(RoleMeta.class);
+                        final String key = meta.key().isEmpty() ? method.getName() : meta.key();
+                        if (meta != null) {
+                            if (method.getParameterCount() == 0) {
+                                final Class<?> returnType = method.getReturnType();
+                                if (returnType == boolean.class) return this.meta().containsKey(key);
+                                else {
+                                    String value = this.meta().get(key);
+                                    if (returnType == String.class) return value;
+                                    if (Enum.class.isAssignableFrom(returnType)) return Enum.valueOf((Class<? extends Enum>) returnType, value);
+
+                                    value = value == null ? "0" : value;
+                                    if (returnType == byte.class) return Byte.valueOf(value);
+                                    if (returnType == short.class) return Short.valueOf(value);
+                                    if (returnType == int.class) return Integer.valueOf(value);
+                                    if (returnType == long.class) return Long.valueOf(value);
+                                    if (returnType == float.class) return Float.valueOf(value);
+                                    if (returnType == double.class) return Double.valueOf(value);
+                                }
+
+                                throw new ClassCastException(String.format("Cannot deserialize value %s with type %s", key, returnType.getName()));
+                            } else {
+                                final Class<?> param = method.getParameters()[0].getType();
+                                final String value = String.valueOf(args[0]);
+
+                                if (param == boolean.class) {
+                                    if (Boolean.parseBoolean(value)) this.meta().put(key, "1");
+                                    else this.meta().remove(key);
+                                } else if (param == Enum.class) this.meta().put(key, ((Enum<?>) args[0]).name());
+                                else this.meta().put(key, value);
+                            }
+                        }
+
+                        return null;
+                    }
+                );
+
+                this.role = instance;
+            } catch (ClassNotFoundException | ClassCastException e) {
                 e.printStackTrace();
             }
         }
